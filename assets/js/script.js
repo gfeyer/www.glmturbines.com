4795cbdf25cb256e496015df321f9bcab7aed1e0
gfeyer <feyervlad@gmail.com>
2025-05-07 16:36:26 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* Theme Script */
+
+var width = screen.width;
+var height = screen.height;
+
+var aspect = height / width
+
+//console.log( aspect )
+
+//document.querySelector('.main-title').style.fontSize = 1 * aspect + 'vh'
